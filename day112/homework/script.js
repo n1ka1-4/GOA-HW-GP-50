247ff5b45fa16fe2a476cfbfc2f08fdb8df73a2a
n1ka1-4 <nikoloz1tefnadze10.19@gmail.com>
2025-06-15 13:33:59 +0400

    commit

--- day112/homework/script.js
+++ day112/homework/script.js
@@ -1,68 +1,68 @@
-// adding task with this function
-function addTask() {
+const addBtn = document.querySelector(".add-btn");
+addBtn.addEventListener("click", () => {
     let input = document.getElementById("task-input");
     let text = input.value.trim();
     if (text === "") return;
+});
 
-    let li = document.createElement("li");
-    let span = document.createElement("span");
-    span.innerText = text;
-    li.appendChild(span);
+let li = document.createElement("li");
+let span = document.createElement("span");
+span.innerText = text;
+li.appendChild(span);
 
-    let buttons = document.createElement("div");
-    buttons.className = "task-buttons";
+let buttons = document.createElement("div");
+buttons.className = "task-buttons";
 
-    // i did like this because i think it is much easyer like that
-    let editBtn = document.createElement("button");
-    editBtn.innerText = "Edit";
-    editBtn.className = "edit-btn";
-    editBtn.onclick = function () {
-        let newText = prompt("Edit task:", span.innerText);
-        if (newText !== null && newText.trim() !== "") {
-            span.innerText = newText.trim();
-        }
+// i did like this because i think it is much easyer like that
+let editBtn = document.createElement("button");
+editBtn.innerText = "Edit";
+editBtn.className = "edit-btn";
+editBtn.onclick = function () {
+    let newText = prompt("Edit task:", span.innerText);
+    if (newText !== null && newText.trim() !== "") {
+        span.innerText = newText.trim();
+    }
+};
+
+// with complite we delete everything in list and add into complite
+let completeBtn = document.createElement("button");
+completeBtn.innerText = "Complete";
+completeBtn.className = "complete-btn";
+completeBtn.onclick = function () {
+    document.getElementById("task-list").removeChild(li);
+    buttons.removeChild(editBtn);
+    buttons.removeChild(completeBtn);
+
+    // i added undo button if someone acidently click complite
+    let undoBtn = document.createElement("button");
+    undoBtn.innerText = "Undo";
+    undoBtn.className = "undo-btn";
+    undoBtn.onclick = function () {
+        document.getElementById("completed-list").removeChild(li);
+        addTaskBack(span.innerText);
     };
+    buttons.insertBefore(undoBtn, deleteBtn);
+    document.getElementById("completed-list").appendChild(li);
+};
 
-    // with complite we delete everything in list and add into complite
-    let completeBtn = document.createElement("button");
-    completeBtn.innerText = "Complete";
-    completeBtn.className = "complete-btn";
-    completeBtn.onclick = function () {
-        document.getElementById("task-list").removeChild(li);
-        buttons.removeChild(editBtn);
-        buttons.removeChild(completeBtn);
+// delete is easy just delete everything
+let deleteBtn = document.createElement("button");
+deleteBtn.innerText = "Delete";
+deleteBtn.className = "delete-btn";
+deleteBtn.onclick = function () {
+    li.remove();
+};
 
-        // i added undo button if someone acidently click complite
-        let undoBtn = document.createElement("button");
-        undoBtn.innerText = "Undo";
-        undoBtn.className = "undo-btn";
-        undoBtn.onclick = function () {
-            document.getElementById("completed-list").removeChild(li);
-            addTaskBack(span.innerText);
-        };
-        buttons.insertBefore(undoBtn, deleteBtn);
-        document.getElementById("completed-list").appendChild(li);
-    };
+// we add buttons
+buttons.appendChild(editBtn);
+buttons.appendChild(completeBtn);
+buttons.appendChild(deleteBtn);
 
-    // delete is easy just delete everything
-    let deleteBtn = document.createElement("button");
-    deleteBtn.innerText = "Delete";
-    deleteBtn.className = "delete-btn";
-    deleteBtn.onclick = function () {
-        li.remove();
-    };
+li.appendChild(buttons);
 
-    // we add buttons
-    buttons.appendChild(editBtn);
-    buttons.appendChild(completeBtn);
-    buttons.appendChild(deleteBtn);
-
-    li.appendChild(buttons);
-
-    // and then this button to our task
-    document.getElementById("task-list").appendChild(li);
-    input.value = "";
-}
+// and then this button to our task
+document.getElementById("task-list").appendChild(li);
+input.value = "";
 
 // this is for undo button
 function addTaskBack(text) {
